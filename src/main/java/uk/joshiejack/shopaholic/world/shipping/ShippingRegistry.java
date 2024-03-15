a77a34ccb6ec16553537db89cdb77cd239d630cf
joshiejack <joshjackwildman@gmail.com>
2024-03-15 01:54:10 +0000

    Switch shipping to a data map

--- src/main/java/uk/joshiejack/shopaholic/world/shipping/ShippingRegistry.java
+++ src/main/java/uk/joshiejack/shopaholic/world/shipping/ShippingRegistry.java
@@ -1,47 +1,35 @@
 package uk.joshiejack.shopaholic.world.shipping;
 
-import it.unimi.dsi.fastutil.objects.Object2LongMap;
-import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
-import uk.joshiejack.penguinlib.event.DatabaseLoadedEvent;
+import net.neoforged.neoforge.registries.datamaps.DataMapType;
+import net.neoforged.neoforge.registries.datamaps.RegisterDataMapTypesEvent;
 import uk.joshiejack.shopaholic.Shopaholic;
 import uk.joshiejack.shopaholic.event.ItemGetValueEvent;
 
-@Mod.EventBusSubscriber(modid = Shopaholic.MODID)
+@Mod.EventBusSubscriber(modid = Shopaholic.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ShippingRegistry {
-    private static final Object2LongMap<Item> itemToValue = new Object2LongOpenHashMap<>();
-    private static final Object2LongMap<ResourceLocation> tagToValue = new Object2LongOpenHashMap<>();
+    public static final DataMapType<Item, Long> SELL_VALUE = DataMapType.builder(new ResourceLocation(Shopaholic.MODID, "sell_value"),
+            Registries.ITEM, Codec.LONG).synced(Codec.LONG, true).build();
 
     @SuppressWarnings("ConstantConditions")
     public static long getValue(ItemStack stack) {
-        long value = stack.hasTag() && stack.getTag().contains("SellValue") ? stack.getTag().getLong("SellValue") : itemToValue.getLong(stack.getItem());
-        if (value == 0) {
-            value = stack.getItemHolder().tags()
-                    .filter(tag -> tagToValue.containsKey(tag.location()))
-                    .mapToLong(tag -> tagToValue.getLong(tag.location()))
-                    .findFirst()
-                    .orElse(0);
-        }
-
+        Long valueObj = stack.hasTag() && stack.getTag().contains("SellValue") ? stack.getTag().getLong("SellValue") :
+                stack.getItemHolder().getData(SELL_VALUE);
+        long value = valueObj != null ? valueObj : 0;
         ItemGetValueEvent event = new ItemGetValueEvent(stack, value);
         NeoForge.EVENT_BUS.post(event);
         return event.getNewValue();
     }
 
     @SubscribeEvent
-    public static void onDatabaseLoaded(DatabaseLoadedEvent event) {
-        itemToValue.clear();
-        tagToValue.clear();
-        event.table("item_values").rows().stream()
-                .filter(row -> row.item() != null && !row.isEmpty("days"))
-                .forEach(row -> itemToValue.put(row.item(), row.getAsLong("days")));
-        event.table("tag_values").rows().stream()
-                .filter(row -> row.getRL("tag") != null && !row.isEmpty("days"))
-                .forEach(row -> tagToValue.put(row.getRL("tag"), row.getAsLong("days")));
+    public static void onDataMap(RegisterDataMapTypesEvent event) {
+        event.register(SELL_VALUE);
     }
 }
--- src/main/java/uk/joshiejack/shopaholic/world/shop/ShopRegistries.java
+++ src/main/java/uk/joshiejack/shopaholic/world/shop/ShopRegistries.java
@@ -1,15 +1,10 @@
 package uk.joshiejack.shopaholic.world.shop;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import uk.joshiejack.shopaholic.api.shop.*;
+import uk.joshiejack.shopaholic.api.shop.CostFormula;
 
 import java.util.Map;
 
 public class ShopRegistries {
-    public static final BiMap<String, Comparator> COMPARATORS = HashBiMap.create();
-    public static final Map<String, Condition> CONDITIONS = new Object2ObjectOpenHashMap<>();
     public static final Map<String, CostFormula> COST_FORMULAE = new Object2ObjectOpenHashMap<>();
-    public static final BiMap<String, ListingType.Builder> LISTING_TYPES = HashBiMap.create();
 }
