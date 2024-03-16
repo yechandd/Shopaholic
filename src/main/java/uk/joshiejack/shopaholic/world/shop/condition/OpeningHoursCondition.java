5613c4795f200e129852648ca681d9bd8ca0a03c
joshiejack <joshjackwildman@gmail.com>
2024-03-16 00:42:35 +0000

    fix hours codec

--- src/main/java/uk/joshiejack/shopaholic/world/shop/condition/OpeningHoursCondition.java
+++ src/main/java/uk/joshiejack/shopaholic/world/shop/condition/OpeningHoursCondition.java
@@ -2,9 +2,7 @@ package uk.joshiejack.shopaholic.world.shop.condition;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.util.StringRepresentable;
 import org.apache.commons.lang3.tuple.Pair;
-import org.jetbrains.annotations.NotNull;
 import uk.joshiejack.penguinlib.util.helper.TimeHelper;
 import uk.joshiejack.shopaholic.api.shop.Condition;
 import uk.joshiejack.shopaholic.api.shop.ShopTarget;
@@ -12,19 +10,11 @@ import uk.joshiejack.shopaholic.api.shop.ShopTarget;
 import javax.annotation.Nonnull;
 import java.time.DayOfWeek;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
-public record OpeningHoursCondition(Map<Weekday, Pair<Integer, Integer>> hours) implements Condition {
-    public enum Weekday implements StringRepresentable {
-        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
-
-        @Override
-        public @NotNull String getSerializedName() {
-            return name().toLowerCase();
-        }
-    }
-
-    public static final Codec<Weekday> DAY_OF_WEEK_CODEC = StringRepresentable.fromEnum(Weekday::values);
+public record OpeningHoursCondition(Map<DayOfWeek, Pair<Integer, Integer>> hours) implements Condition {
+    public static final Codec<DayOfWeek> DAY_OF_WEEK_CODEC = Codec.STRING.xmap((d) -> DayOfWeek.valueOf(d.toUpperCase(Locale.ENGLISH)), (d) -> d.name().toLowerCase(Locale.ENGLISH));
     public static final Codec<OpeningHoursCondition> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.unboundedMap(DAY_OF_WEEK_CODEC, TimeCondition.TIME_CODEC).fieldOf("hours").forGetter(condition -> condition.hours)
     ).apply(instance, OpeningHoursCondition::new));
@@ -51,18 +41,13 @@ public record OpeningHoursCondition(Map<Weekday, Pair<Integer, Integer>> hours) 
 
     //Conditions
     public static class Builder {
-        protected final Map<Weekday, Pair<Integer, Integer>> hours = new HashMap<>();
+        protected final Map<DayOfWeek, Pair<Integer, Integer>> hours = new HashMap<>();
         public Condition build() {
             return new OpeningHoursCondition(hours);
         }
 
-        public Builder withHours(Weekday day, int opening, int closing) {
-            hours.put(day, Pair.of(opening, closing));
-            return this;
-        }
-
         public Builder withHours(DayOfWeek day, int opening, int closing) {
-            return withHours(Weekday.valueOf(day.name()), opening, closing);
+            return withHours(day, opening, closing);
         }
     }
 }
